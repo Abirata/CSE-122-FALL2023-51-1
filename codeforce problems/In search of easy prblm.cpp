979ae8e36713a0fb3c62bd253ed5f1944b008709
Abirata Chakma <139255437+Abirata@users.noreply.github.com>
2024-03-27 19:18:49 +0600

    Create In search of easy prblm.cpp

---
+++ codeforce problems/In search of easy prblm.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, i, count=0;
+    cin>>n;
+ 
+    int arr[n];
+ 
+    for(i=0; i<n; i++)
+    {
+        cin>>arr[i];
+       if(arr[i]==1)
+       {
+           count++;
+       }
+    }
+     if(count==0)
+            cout<<"EASY"<<endl;
+     else
+            cout<<"HARD"<<endl;
+    return 0;
+}
